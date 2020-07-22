96f21fef45d43e029414078446b9d32830c319fa
baryal1299 <bishwash.aryal@gmail.com>
2020-07-22 11:18:58 -0400

    temporary fix for vue environment variable not being resolved

--- client/src/services/HttpCommon.ts
+++ client/src/services/HttpCommon.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+    // baseURL: process.env.VUE_APP_API_URL,
+    baseURL: "http://localhost:8080/api/v1",
     headers: {
         "Content-type": "application/json"
     }
